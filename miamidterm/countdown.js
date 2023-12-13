ad500afd9ac1502ee3db530eda15c8f2ca23315b
miadevera <144238379+miadevera@users.noreply.github.com>
2023-12-13 04:23:30 -0500

    FINAL

--- miamidterm/countdown.js
+++ miamidterm/countdown.js
@@ -1,21 +1,28 @@
-let countDownDate = new Date("2024-04-04T00:00:00").getTime();
+document.addEventListener('DOMContentLoaded', (event) => {
+    const textArray = ['Please','scroll', 'down', 'to', 'access', 'link', 'to', 'buy', 'my', 'art!'];
+    let i = 0;
 
-let interval = setInterval(function() {
-    let now = new Date().getTime();
-    let distance = countDownDate - now;
-
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    document.getElementById("days").innerText = days;
-    document.getElementById("hours").innerText = hours;
-    document.getElementById("minutes").innerText = minutes;
-    document.getElementById("seconds").innerText = seconds;
-
-    if (distance < 0) {
-        clearInterval(interval);
-        document.getElementById("countdown").innerText = "EXPIRED";
+    function animateText() {
+        if(i < textArray.length) {
+            document.getElementById('animated-text').innerHTML += textArray[i] + ' ';
+            i++;
+        }
     }
-}, 1000);
+    setInterval(animateText, 500); // 500ms delay between each word
+});
+
+var follower = document.getElementById('mouse');
+var tween = gsap.to(follower, {
+  x: 0,
+  y: 0,
+  duration: 0.5,
+  ease: "power2",
+  paused: true
+});
+
+document.body.addEventListener('mousemove', function(e) {
+  tween.vars.x = e.clientX;
+  tween.vars.y = e.clientY;
+  tween.invalidate();
+  tween.restart();
+});
